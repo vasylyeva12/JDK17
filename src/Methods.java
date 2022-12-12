b7f4bb247cddac7000f08ee2cbd0cc4e9c77d0f9
vasylyeva12 <aloyna.vasilieva@gmail.com>
2022-12-13 00:48:17 +0200

    Initial commit

---
+++ src/Methods.java
@@ -0,0 +1,33 @@
+public class Methods {
+    public static void main(String[] args) {
+        byte a = 7;
+        System.out.println("Byte - " + a);
+        Short a1 = 2022;
+        System.out.println("Short - " + a1);
+
+        int b = 15000000;
+        System.out.println("Это целое число - " + b);
+        long lg = 25000000000l;
+        System.out.println("Long -" + lg);
+        char ch = 's';
+        System.out.println("Char - " + ch);
+
+        double c = 123.456;
+        System.out.println("Это дробное число - " + c);
+        float fl = 33.34556f;
+        System.out.println("Float -" + fl);
+        boolean student = true;
+        System.out.println("Boolean - " + student );
+
+        String d ="This is a string variable";
+        System.out.println("Text - " + d);
+
+
+
+        int f = 389;
+
+        System.out.print(f/100 + " " + f/10%10 + " " + f%10);
+
+
+    }
+}
